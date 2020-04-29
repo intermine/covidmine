cb147787bd301fbb140b5ec91dd58a4846f59251
sergio contrino <contrino.it@gmail.com>
2020-04-29 18:17:25 +0100

    now with working functions

--- docovid.sh
+++ docovid.sh
@@ -7,6 +7,7 @@
 # default settings: edit with care
 INTERACT=n       # y: step by step interaction
 SWAP=y           # n: don't swap db
+GETDATA=y        # n: don't update uniprot and gff
 
 progname=$0
 
@@ -14,7 +15,8 @@ function usage () {
 	cat <<EOF
 
 Usage:
-$progname [-i] [-s]
+$progname [-d] [-i] [-s]
+	-d: no checking of sources (uniprot and gff) for update
 	-i: interactive mode
 	-s: no swapping of build db (for example after a build fail)
 	-v: verbode mode
@@ -31,10 +33,9 @@ EOF
 }
 
 
-
-
-while getopts "is" opt; do
+while getopts "dis" opt; do
    case $opt in
+	d )  echo "- Don't mirror sources" ; GETDATA=n;;
 	i )  echo "- Interactive mode" ; INTERACT=y;;
         s )  echo "- Don't swap db" ; SWAP=n;;
         h )  usage ;;
@@ -43,10 +44,11 @@ while getopts "is" opt; do
 done
 
 shift $(($OPTIND - 1))
+
 PDIR=$HOME/.intermine
 COV=covidmine.properties
 COVDIR=/micklem/data/thalemine/git/covidmine
-
+DATADIR=/micklem/data/covid
 
 function interact {
 # if testing, wait here before continuing
@@ -61,36 +63,65 @@ fi
 
 function swap {
 # to change build db
-
+echo "AO!"
 cd $PDIR
+pwd
 USE=gin
 CURR=`ls -l covidmine.properties | cut -f2 -d">" | cut -f3 -d.`
 if [ $CURR = "gin" ]
-then 
+then
 USE=tonic
 fi
-
+echo $USE
 export DD=`date "+%d-%m-%Y %H.%M"`
-sed -i 's/preAlpha.*/preAlpha \<i\>'"$USE"'\<\/i\> '"$DD"'/' $COV.$USE 
+sed -i 's/preAlpha.*/preAlpha \<i\>'"$USE"'\<\/i\> '"$DD"'/' $COV.$USE
 
 rm $COV
 ln -s $COV.$USE $COV
 
-echo $CURR" -> "$USE 
+echo $CURR" -> "$USE
+
+}
+
+function getSources {
+# do the mirroring!
+# TODO: avoid the gff expansion if no changes
+
+FTPROT=ftp://ftp.uniprot.org/pub/databases/uniprot/pre_release/
+UNIDIR=$DATADIR/uniprot
+FASDIR=$DATADIR/fasta
+GFFDIR=$DATADIR/gff
+
+GFFILE=GCF_009858895.2_ASM985889v3_genomic
+
+# fix for now
+cd $GFFDIR/mirror
+wget -N https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/009/858/895/GCF_009858895.2_ASM985889v3/GCF_009858895.2_ASM985889v3_genomic.gff.gz
+mv $GFFDIR/GCF_009858895.2_ASM985889v3_genomic.gff $GFFDIR/oldies/GCF_009858895.2_ASM985889v3_genomic.gff.`date "+%y%m%d.%H%M"`
+gzip -d -c *.gff.gz > $GFFDIR/GCF_009858895.2_ASM985889v3_genomic.gff
+
+cd $UNIDIR
+wget -N $FTPROT/*.fasta
+wget -N $FTPROT/covid-19.xml
 
 }
 
 
+
 if [ $SWAP = "y" ]
 then
-interact "Swapping build db"
+   interact "Swapping build db"
+   swap
+fi
+
+if [ $GETDATA = "y" ]
+then
+   interact "Getting sources"
+   getSources
 fi
 
 
 cd $COVDIR
-
-#interact "Getting sources"
-#./getSources.sh
 
 interact "Building.."
 
